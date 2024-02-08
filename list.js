5a9f59d2bc6c7293d40c664d9c31b28d2d2ae902
AlanGael19264 <156270744+AlanGael19264@users.noreply.github.com>
2024-02-07 20:56:37 -0600

    Update list.js

--- list.js
+++ list.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         clone.querySelector("[data-id='name']").textContent = "Rick Sanchez";
         clone.querySelector("[data-id='status']").textContent = "Alive";
         clone.querySelector("[data-id='specie']").textContent= "Human";
-        clone.querySelector("[data-id='image']").src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmag.elcomercio.pe%2Ffama%2Frick-and-morty-explicacion-del-verdadero-origen-de-rick-sanchez-serie-de-hbo-max-nnda-nnlt-noticia%2F&psig=AOvVaw2XaaPYgxvF84Q5X7Daztcj&ust=1707446929456000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJD-m9DdmoQDFQAAAAAdAAAAABAE";
+        clone.querySelector("[data-id='image']").src = "https://rickandmortyapi.com/api/character/avatar/1.jpeg";
         itemList.appendChild(clone);
     });
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
