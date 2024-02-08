77f3b1ad94aec876d8f21a74a5cb2af986639c7b
AlanGael19264 <156270744+AlanGael19264@users.noreply.github.com>
2024-02-07 20:33:24 -0600

    Update list.js

--- list.js
+++ list.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         clone.querySelector("[data-id='name']").textContent = "Rick Sanchez";
         clone.querySelector("[data-id='status']").textContent = "Alive";
         clone.querySelector("[data-id='specie']").textContent= "Human";
-        clone.querySelector("[data-id='image']").textContent= "https://static.wikia.nocookie.net/rick-and-morty933/images/a/a6/Rick_Sanchez.png/revision/latest/thumbnail/width/360/height/360?cb=20170925123206&path-prefix=de";
+        clone.querySelector("[data-id='image']").textContent= "src='https://static.wikia.nocookie.net/rick-and-morty933/images/a/a6/Rick_Sanchez.png/revision/latest/thumbnail/width/360/height/360?cb=20170925123206&path-prefix=de';"
         itemList.appendChild(clone);
     });
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
