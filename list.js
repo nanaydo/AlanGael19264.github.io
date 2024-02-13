f1e1c93c46b7bc5835e807aeb532e5487e7774ea
AlanGael19264 <156270744+AlanGael19264@users.noreply.github.com>
2024-02-12 20:57:41 -0600

    Update list.js

--- list.js
+++ list.js
@@ -1,24 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".btn.btn-primary").addEventListener("click", event => {
-        var itemList = document.getElementById("my-list");
-        var template = document.getElementById("list-template");
-        var total = itemList.childElementCount + 1;
-        var clone = template.content.cloneNode(true);
-        clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='name']").textContent = "Rick Sanchez";
-        clone.querySelector("[data-id='status']").textContent = "Alive";
-        clone.querySelector("[data-id='specie']").textContent= "Human";
-        clone.querySelector("[data-id='image']").src = "https://rickandmortyapi.com/api/character/avatar/1.jpeg";
-        itemList.appendChild(clone);
-    });
-    document.querySelector(".btn.btn-light").addEventListener("click", event => {
-        var itemList = document.getElementById("my-list");
-        itemList.replaceChildren();
-    });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector(".btn.btn-primary").addEventListener("click", event => {
         fetch('https://rickandmortyapi.com/api/character')
             .then((response) => {
                 if (!response.ok) {
